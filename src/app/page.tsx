447c7d13f1936306f782af770fbddeb56dfa5b49
Артем Кондратенко <lord,kazakovv@gmail.com>
2026-02-04 09:41:20 +0200

    fix(main page): clean file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,13 @@
-import Test from "@/components/test";
 import Header from "@/components/sections/header";
-import Console from "@/components/ui/console";
-import Browser from "@/components/ui/browser";
-import AboutCards from "@/components/sections/cardsWork";
 import Footer from "@/components/sections/footer";
-import Timeline from "@/components/sections/timeline";
-import BrowserWindow from "@/components/ui/browserTest";
-import Info from "@/components/sections/info";
 import Main from "@/components/sections/main";
-
-
 
 export default function Home() {
   return (
     <>
       <Header />
-     <Main />
-      
+      <Main />
       <Footer />
-      {/* <Test /> */}
-
     </>
 
   );
